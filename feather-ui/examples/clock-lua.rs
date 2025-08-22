1268bc4701391eef76b554a1df44d642b551a129
Erik McClure <erikm@fundament.software>
2025-08-21 22:53:46 -0700

    Finish DSL

--- feather-ui/examples/clock-lua.rs
+++ feather-ui/examples/clock-lua.rs
@@ -1,17 +1,36 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2025 Fundament Research Institute <https://fundament.institute>
 
+use std::sync::atomic::{AtomicUsize, Ordering};
+
 use feather_ui::lua::LuaApp;
 use mlua::{FromLua, Lua, UserData, UserDataFields};
 
 const LAYOUT: &[u8] = include_bytes!("./clock.lua");
 
-#[derive(PartialEq, Clone, Debug)]
-struct TimeState {}
+#[derive(Debug)]
+struct TimeState {
+    count: AtomicUsize,
+}
+
+impl PartialEq for TimeState {
+    fn eq(&self, other: &Self) -> bool {
+        self.count.load(Ordering::Relaxed) == other.count.load(Ordering::Relaxed)
+    }
+}
+
+impl Clone for TimeState {
+    fn clone(&self) -> Self {
+        Self {
+            count: self.count.load(Ordering::Relaxed).into(),
+        }
+    }
+}
 
 impl UserData for TimeState {
     fn add_fields<F: UserDataFields<Self>>(f: &mut F) {
-        f.add_field_method_get("time_hour", |_, _| {
+        f.add_field_method_get("time_hour", |_, s| {
+            s.count.fetch_add(1, Ordering::Relaxed);
             let day = std::time::SystemTime::now()
                 .duration_since(std::time::UNIX_EPOCH)
                 .unwrap()
@@ -19,7 +38,8 @@ impl UserData for TimeState {
                 % (24 * 60 * 60);
             Ok(day / (60 * 60))
         });
-        f.add_field_method_get("time_min", |_, _| {
+        f.add_field_method_get("time_min", |_, s| {
+            s.count.fetch_add(1, Ordering::Relaxed);
             let day = std::time::SystemTime::now()
                 .duration_since(std::time::UNIX_EPOCH)
                 .unwrap()
@@ -27,7 +47,8 @@ impl UserData for TimeState {
                 % (24 * 60 * 60);
             Ok((day / 60) % 60)
         });
-        f.add_field_method_get("time_sec", |_, _| {
+        f.add_field_method_get("time_sec", |_, s| {
+            s.count.fetch_add(1, Ordering::Relaxed);
             let day = std::time::SystemTime::now()
                 .duration_since(std::time::UNIX_EPOCH)
                 .unwrap()
@@ -56,7 +77,8 @@ fn main() {
     let lua = Lua::new();
 
     let (mut app, event_loop) =
-        LuaApp::<TimeState>::new::<()>(&lua, TimeState {}, Vec::new(), LAYOUT).unwrap();
+        LuaApp::<TimeState>::new::<()>(&lua, TimeState { count: 0.into() }, Vec::new(), LAYOUT)
+            .unwrap();
 
     event_loop.run_app(&mut app).unwrap();
 }
